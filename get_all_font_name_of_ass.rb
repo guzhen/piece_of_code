791b9ec598fb1e7a0c7e4d433ed698789cfbd7f3
guzhen <guzhen@users.noreply.github.com>
2015-10-13 11:29:08 +0800

    merge f1 f2

--- get_all_font_name_of_ass.rb
+++ get_all_font_name_of_ass.rb
@@ -1,6 +1,6 @@
 data=File.new(ARGV[0]).read().force_encoding('utf-8')
 style=Regexp.new('^Style:\s+[^,]+,([^,]+),')
 script=Regexp.new('{\\\\fn([^}\\\\]+)[^}]*}')
-f1=data.scan(style).flatten.uniq.join("\n")
-f2=data.scan(script).flatten.uniq.join("\n")
-File.new(ARGV[0]+"_font.txt",'w').write(f1+"\n"+f2)
+f1=data.scan(style).flatten.uniq
+f2=data.scan(script).flatten.uniq
+File.new(ARGV[0]+"_font.txt",'w').write((f1+f2).uniq.join("\n"))
