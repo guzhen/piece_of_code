a5eaf487005687b0d9abc5a638983cf9f1220a20
guzhen <guzhen@users.noreply.github.com>
2015-12-13 22:19:44 +0800

    Update get_all_font_name_of_ass.rb

--- get_all_font_name_of_ass.rb
+++ get_all_font_name_of_ass.rb
@@ -1,6 +1,6 @@
 data=File.new(ARGV[0]).read().force_encoding('utf-8')
 style=Regexp.new('^Style:\s+[^,]+,([^,]+),')
-script=Regexp.new('{\\\\fn([^}\\\\]+)[^}]*}')
+script=Regexp.new('{[^}]*\\\\fn([^}\\\\]+)[^}]*}')
 f1=data.scan(style).flatten.uniq
 f2=data.scan(script).flatten.uniq
 File.new(ARGV[0]+"_font.txt",'w').write((f1+f2).uniq.join("\n"))
